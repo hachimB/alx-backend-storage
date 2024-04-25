2e08bce2ff87af044be21ee4512d9f07d62705ea
hachimB <boubacarhachim@gmail.com>
2024-04-25 23:26:04 +0100

    Optimize search and score

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,4 @@
+-- A SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score.
+-- Optimize search and score
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
