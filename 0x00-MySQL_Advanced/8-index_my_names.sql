c63b0189fbfe775687271e710ed1b18f93cacf5a
hachimB <boubacarhachim@gmail.com>
2024-04-25 20:00:15 +0100

    INDEX

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -5,22 +5,4 @@
 -- ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
 
 -- CREATE INDEX idx_name_first ON names (first_letter);
-DELIMITER //
-CREATE PROCEDURE add_column_if_not_exists()
-BEGIN
-    DECLARE _count INT;
-    SELECT COUNT(*) INTO _count
-    FROM information_schema.columns 
-    WHERE table_name = 'names' AND column_name = 'first_letter';
-    IF _count = 0 THEN
-        ALTER TABLE names
-        ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
-    END IF;
-END//
-DELIMITER ;
-
-CALL add_column_if_not_exists();
-
-DROP PROCEDURE add_column_if_not_exists;
-
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
