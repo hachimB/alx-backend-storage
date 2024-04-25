da4b73512b227d3320df9fc27c1a7babf1f8edfd
hachimB <boubacarhachim@gmail.com>
2024-04-25 19:37:16 +0100

    INDEX

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -3,4 +3,4 @@
 
 ALTER TABLE names
 ADD COLUMN  first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
-CREATE INDEX idx_name_first ON names(first_letters)
+CREATE INDEX idx_name_first ON names(first_letter);
