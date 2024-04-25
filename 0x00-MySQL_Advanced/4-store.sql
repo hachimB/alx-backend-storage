646ee990d1a729de80bfc1ddbbc3c88de96356d9
hachimB <boubacarhachim@gmail.com>
2024-04-25 04:27:15 +0100

    TRIGGERS

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,4 +4,5 @@ CREATE TRIGGER after_add_new_order
 AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items
-SET quantity = quantity + 1;
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
