553516505f8d836b6e225872b6382c48c277f984
hachimB <boubacarhachim@gmail.com>
2024-05-01 16:12:57 +0100

    redis.Redis().set()

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+import redis
+import uuid
+import typing
+
+
+r = redis.Redis()
+class Cache:
+    """Cache class"""
+
+    def __init__(self):
+        """init method"""
+        self._redis = r
+
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+        """store method"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
