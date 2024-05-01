b8b7cb76bde83b9a35fd4f86658e7996fb4a8c1b
hachimB <boubacarhachim@gmail.com>
2024-05-01 17:08:30 +0100

    fn

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: typing.Optional[callable] = None):
+        """Get method"""
+        result = self._redis.get(key)
+        if result is None:
+            return None
+        if fn:
+            return fn(result)
+        return result
+    
+    def get_str(self, key: str) -> str:
+        """Get string method"""
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> typing.Optional[int]:
+        """Get integer method"""
+        return self.get(key, fn=int)
